a7c73ee25b541b65444a2eb6c2c3b5170eb775b9
root <root@DESKTOP-5N8E6NA.>
2025-04-23 01:47:06 +0200

    99% done only include the data managment time for both containers

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -14,11 +14,17 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &assign)
     return *this;
 }
 
-void PmergeMe::loadVector(char **to_conv)
+void PmergeMe::print_before()
 {
-    _has_odd = false;
+    for(unsigned int i = 0; i < _orig.size(); i++)
+    {
+        std::cout << _orig[i] << " ";
+    }
+    std::cout << std::endl;
+}
 
-    
+bool PmergeMe::fillVecAndDeq(char **to_conv)
+{
     if (to_conv[0] && !isdigit(to_conv[0][0]))
         to_conv++;
     for (unsigned int i = 0; to_conv[i]; i++)
@@ -28,38 +34,50 @@ void PmergeMe::loadVector(char **to_conv)
             if(to_conv[i][j] == '"' || to_conv[i][j] == '\'')
             {
                 std::cerr << "Error: Invalid input, it only can be digits !" << std::endl;
-                return;
+                return false;
             }
-            if(!isdigit(to_conv[i][j]))
+            else if(atoll(to_conv[i]) < 0)
             {
-                std::cerr << "Error: Invalid input, it only can be digits !" << std::endl;
-                return;
+                std::cerr << "Error: Invalid input, it only can be positive integers !" << std::endl;
+                return false;
+            }
+            else if(!isdigit(to_conv[i][j]))
+            {
+                std::cerr << "Error: Invalid input, it only can be digits or numbers !" << std::endl;
+                return false;
             }
         }
         if (atoll(to_conv[i]) > 2147483647)
         {
             std::cerr << "Error: Input too big !" <<  std::endl;
-            return;
+            return false;
         }
         _orig.push_back(atoll(to_conv[i]));
-        
+        _deq.push_back(atoll(to_conv[i]));        
     }
     if (_orig.size() % 2 != 0)
         _has_odd = true;
-    for (unsigned int i = 0; i < _orig.size(); i++)
-    {
-        if (_orig[i] < 0)
-        {
-            std::cerr << "Error: The numbers should only be positive !" << std::endl;
-            return;
-        }
-    }
+    return true;
+}
+
+void PmergeMe::loadVectorDeque(char **to_conv)
+{
+    _has_odd = false;
+
+    if(!fillVecAndDeq(to_conv))
+        return;
+    std::cout << "Before: ";
+    print_before();
     for (unsigned int i = 0; i < _orig.size() - 1; i += 2)
         _vec.push_back(std::make_pair(_orig[i], _orig[i + 1]));
+    for (unsigned int i = 0; i < _deq.size() - 1; i += 2)
+        _pair_deque.push_back(std::make_pair(_deq[i], _deq[i + 1]));
     if (_has_odd)
     {
         _the_od_elem = _orig[_orig.size() - 1];
+        _the_od_elem_for_deck = _deq[_deq.size() - 1];
         _orig.pop_back();
+        _deq.pop_back();
     }
     startFordJhon();
 }
@@ -114,6 +132,58 @@ PmergeMe::startRecMerging(std::vector<std::pair<unsigned int, unsigned int> > ve
     return MergeCompPairs(left, right);
 }
 
+
+std::deque<std::pair<unsigned int, unsigned int> > PmergeMe::MergeCompPairsForDeque(std::deque<std::pair<unsigned int, unsigned int> > left, std::deque<std::pair<unsigned int, unsigned int> > right)
+{
+
+    std::deque<std::pair<unsigned int, unsigned int> >::iterator left_it = left.begin();
+    std::deque<std::pair<unsigned int, unsigned int> >::iterator right_it = right.begin();
+    std::deque<std::pair<unsigned int, unsigned int> > merged;
+
+    while (left_it != left.end() && right_it != right.end())
+    {
+        if (left_it->first <= right_it->first)
+        {
+            merged.push_back(*left_it);
+            ++left_it;
+        }
+        else
+        {
+            merged.push_back(*right_it);
+            ++right_it;
+        }
+    }
+    while (left_it != left.end())
+    {
+        merged.push_back(*left_it);
+        ++left_it;
+    }
+    while (right_it != right.end())
+    {
+        merged.push_back(*right_it);
+        ++right_it;
+    }
+    return merged;
+}
+
+
+std::deque<std::pair<unsigned int, unsigned int> >
+PmergeMe::startRecMergingForDeque(std::deque<std::pair<unsigned int, unsigned int> > deque)
+{
+
+    if (deque.size() <= 1)
+    {
+        return (deque);
+    }
+    size_t mid = deque.size() / 2;
+    std::deque<std::pair<unsigned int, unsigned int> > left(deque.begin(), deque.begin() + mid);
+    std::deque<std::pair<unsigned int, unsigned int> > right(deque.begin() + mid, deque.end());
+
+    left = startRecMergingForDeque(left);
+    right = startRecMergingForDeque(right);
+    return MergeCompPairsForDeque(left, right);
+}
+
 unsigned int binarySearch(std::vector<unsigned int> &vector, unsigned int target)
 {
     if (vector.empty())
@@ -135,6 +205,53 @@ unsigned int binarySearch(std::vector<unsigned int> &vector, unsigned int target
         }
     }
     return left;
+}
+
+unsigned int binarySearchForDeque(std::deque<unsigned int> &vector, unsigned int target)
+{
+    if (vector.empty())
+        return (0);
+    int left = 0;
+    int right = vector.size() - 1;
+    while (left <= right)
+    {
+        unsigned int mid = (left + right) / 2;
+        if (vector[mid] == target)
+            return mid;
+        else if (target > vector[mid])
+            left = mid + 1;
+        else
+        {
+            if (right == 0)
+                break;
+            right = mid - 1;
+        }
+    }
+    return left;
+}
+
+
+std::deque<unsigned int> generateJacobNumbersDeq(unsigned int size)
+{
+    std::deque<unsigned int> result;
+    if (size <= 1)
+    {
+        return result;
+    }
+    result.push_back(1);
+    unsigned int j_fst = 1;
+    unsigned int j_scnd = 1;
+    while (result.size() < size)
+    {
+        unsigned int j_next = j_fst + 2 * j_scnd;
+        j_scnd = j_fst;
+        j_fst = j_next;
+        result.push_back(j_next);
+        
+    }
+    while (!result.empty() && result.back() > size) //ensure not to exced pend size
+        result.pop_back();
+    return result;
 }
 
 std::vector<unsigned int> generateJacobNumbers(unsigned int size)
@@ -159,6 +276,39 @@ std::vector<unsigned int> generateJacobNumbers(unsigned int size)
         result.pop_back();
     return result;
 }
+
+std::deque<unsigned int> startJhonInsertDeq(std::deque<unsigned int> MainChain, std::deque<unsigned int> &Pend, std::deque<unsigned int> &JachobtalNUmbs)
+{
+    int pos = 0;
+    int j = 0;
+    std::deque<bool>used(Pend.size(), false);
+
+    if (MainChain.empty())
+        return MainChain;
+    if (Pend.empty())
+        return MainChain;
+    for (unsigned int i = 0; i < JachobtalNUmbs.size(); i++)
+    {
+        j = JachobtalNUmbs[i] - 1;
+        if (j < static_cast<int>(Pend.size()))
+        {   
+            pos = binarySearchForDeque(MainChain, Pend[j]);
+            MainChain.insert(MainChain.begin() + pos, Pend[j]);
+            used[j] = true;
+        }
+    }
+    for (unsigned int i = 0; i < Pend.size(); i++)
+    {
+        if (!used[i])
+        {
+            pos = binarySearchForDeque(MainChain, Pend[i]);
+            MainChain.insert(MainChain.begin() + pos, Pend[i]);
+        }
+    }
+    return MainChain;
+}
+
+
 
 std::vector<unsigned int> startJhonInsert(std::vector<unsigned int> MainChain, std::vector<unsigned int> &Pend, std::vector<unsigned int> &JachobtalNUmbs)
 {
@@ -191,13 +341,15 @@ std::vector<unsigned int> startJhonInsert(std::vector<unsigned int> MainChain, s
     return MainChain;
 }
 
-void PmergeMe::startFordJhon()
+void PmergeMe::StartVector()
 {
     std::vector<unsigned int> mainChain;
     std::vector<unsigned int> sorte;
     std::vector<unsigned int> Pend;
-    
+
     std::vector<std::pair<unsigned int, unsigned int> >::iterator it;
+    std::clock_t start = std::clock();
+
     for (it = _vec.begin(); it != _vec.end(); ++it)
     {
         if (it->first < it->second)
@@ -213,12 +365,11 @@ void PmergeMe::startFordJhon()
     }
     std::vector<unsigned int> JachobtalNUmbs = generateJacobNumbers(Pend.size());
     mainChain = startJhonInsert(mainChain, Pend, JachobtalNUmbs);
+
     if (_has_odd)
     {
         if (mainChain.empty())
-        {
             mainChain.push_back(_the_od_elem);
-        }
         else
         {
             unsigned int pos = binarySearch(mainChain, _the_od_elem);
@@ -226,11 +377,69 @@ void PmergeMe::startFordJhon()
                 mainChain.insert(mainChain.begin() + pos, _the_od_elem);
         }
     }
+    std::clock_t end = std::clock();
+    double time = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+    
+    // std::cout << "Vector is: ";
+    std::cout << "After: ";
     for (unsigned int i = 0; i < mainChain.size(); i++)
     {
         std::cout << mainChain[i]  << " ";
     }
     std::cout << std::endl;
+    std::cout << "Time to process a range of " << mainChain.size() << " elements with std::vector : " << time * 10 << " us" << std::endl; 
+}
+
+void PmergeMe::StartDeque()
+{
+    std::deque<unsigned int> mainChain_deq;
+    std::deque<unsigned int> sorte_deq;
+    std::deque<unsigned int> Pend_deq;
+    
+    std::deque<std::pair<unsigned int, unsigned int> >::iterator it_deq;
+    std::clock_t start = std::clock();
+    
+    for (it_deq = _pair_deque.begin(); it_deq != _pair_deque.end(); ++it_deq)
+    {
+        if (it_deq->first < it_deq->second)
+        {
+            std::swap(it_deq->first, it_deq->second);
+        }
+    }
+    _pair_deque = startRecMergingForDeque(_pair_deque);
+    for (it_deq = _pair_deque.begin(); it_deq != _pair_deque.end(); ++it_deq)
+    {
+        mainChain_deq.push_back(it_deq->first);
+        Pend_deq.push_back(it_deq->second);
+    }
+    std::deque<unsigned int> JachobtalNUmbsDeq = generateJacobNumbersDeq(Pend_deq.size());
+    mainChain_deq = startJhonInsertDeq(mainChain_deq, Pend_deq, JachobtalNUmbsDeq);
+    if (_has_odd)
+    {
+        if (mainChain_deq.empty())
+            mainChain_deq.push_back(_the_od_elem_for_deck);
+        else
+        {
+            unsigned int pos_deq = binarySearchForDeque(mainChain_deq, _the_od_elem_for_deck);
+            if (pos_deq <= mainChain_deq.size())
+                mainChain_deq.insert(mainChain_deq.begin() + pos_deq, _the_od_elem_for_deck);
+        }
+    }
+    std::clock_t end = std::clock();
+    double time = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+    // std::cout << "Deque is: ";
+    // for (unsigned int i = 0; i < mainChain_deq.size(); i++)
+    // {
+    //     std::cout << mainChain_deq[i]  << " ";
+    // }
+    // std::cout << std::endl;
+    std::cout << "Time to process a range of " << mainChain_deq.size() << " elements with std::deque : " << time * 10 << " us" << std::endl; 
+}
+
+void PmergeMe::startFordJhon()
+{
+    StartVector();
+    StartDeque();
 }
 
 PmergeMe::~PmergeMe() {}
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -8,22 +8,31 @@
 #include <deque>
 #include <fstream>
 #include <sstream>
+#include <ctime>
 
 class PmergeMe
 {
 private:
     std::vector<std::pair<unsigned int, unsigned int> > _vec;
-    std::deque<std::pair<unsigned int, unsigned int> > _deque;
+    std::deque<std::pair<unsigned int, unsigned int> > _pair_deque;
     std::vector<long long> _orig;
+    std::vector<long long> _deq;
     bool _has_odd;
     unsigned int _the_od_elem;
+    unsigned int _the_od_elem_for_deck;
 public:
     PmergeMe();
     PmergeMe(const PmergeMe &copy);
     PmergeMe &operator=(const PmergeMe &assign);
-    void loadVector(char **to_conv);
+    void loadVectorDeque(char **to_conv);
+    void StartVector();
+    void StartDeque();
+    void print_before();
+    bool fillVecAndDeq(char **to_conv);
     std::vector<std::pair<unsigned int, unsigned int> > startRecMerging(std::vector<std::pair<unsigned int, unsigned int> > vec);
+    std::deque<std::pair<unsigned int, unsigned int> > startRecMergingForDeque(std::deque<std::pair<unsigned int, unsigned int> > deque);
     std::vector<std::pair<unsigned int, unsigned int> > MergeCompPairs(std::vector<std::pair<unsigned int, unsigned int> > left, std::vector<std::pair<unsigned int, unsigned int> > right);
+    std::deque<std::pair<unsigned int, unsigned int> > MergeCompPairsForDeque(std::deque<std::pair<unsigned int, unsigned int> > left, std::deque<std::pair<unsigned int, unsigned int> > right);
     void startFordJhon();
     ~PmergeMe();
 };
